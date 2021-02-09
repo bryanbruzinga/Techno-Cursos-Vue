27ec31b062a27517246e71e4d0dc2cbf9b8a0a19
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-02-09 09:22:55 -0300

    inclusão do projeto em repositório separado

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <TheHeader/>
+    <router-view></router-view>
+  </div>
+    
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue"
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+<style>
+body {
+    margin: 0;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #234;
+}
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+a {
+    font-weight: bold;
+    text-decoration: none;
+    color: #234;
+}
+
+a.router-link-active {
+    color: #4b8;
+}
+</style>
