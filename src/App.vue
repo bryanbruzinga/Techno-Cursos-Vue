bae603729273358ae3da3ca005ddb78703237d89
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-02-09 15:50:19 -0300

    página Home finalizada

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,10 @@ a {
     color: #234;
 }
 
+img {
+  max-width: 100%;
+}
+
 a.router-link-active {
     color: #4b8;
 }
@@ -41,5 +45,26 @@ a.router-link-active {
   max-width: 800px;
   margin: 0 auto;
   padding: 20px;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+
+.v-enter-active {
+  transition: all .3s;
+}
+
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(200px, 400px);
+  gap: 30px;
+}
+
+@media screen and (max-width: 400px) {
+  .conteudo {
+    display: block;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
+
     <div v-if="loading">
       <PageLoading/>
     </div>
-    <div v-if="api">
-      <h1>Home</h1>
-      <p>{{api}}</p>
-    </div>   
+
+    <transition>
+      <div v-if="api" class="conteudo">
+        <div>
+          <h1>Sobre a {{api.titulo}}</h1>
+          <p>{{api.descricao}}</p>
+          <router-link class="btn-cursos" tag="button" to="/cursos">Cursos</router-link>
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                <p>{{avaliacao.nome}}</p>
+                <p>{{avaliacao.descricao}}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src="@/assets/aprender.png" alt="Aprenda Web Design">
+      </div>
+    </transition>
+
   </div>
 </template>
 
@@ -22,3 +40,19 @@ export default {
   }
 };
 </script>
+
+<style>
+.btn-cursos {
+  border: none;
+  background: #4b8;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  box-shadow: 0 4px 2px rgba(0,0,0, .1);
+  padding: 15px 40px;
+  font-size: 1rem;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
